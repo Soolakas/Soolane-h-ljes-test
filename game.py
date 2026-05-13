99f99f16119a88f504869b6335a750ec9c9345a6
Soolakas <robin.markus.loos@gmail.com>
2026-05-14 01:20:39 +0300

    Balance #2

--- game.py
+++ game.py
@@ -281,6 +281,7 @@ def on_upgrade_selected(index):
     choices = upgrade_manager.pending_choices
     if 0 <= index < len(choices):
         upgrade_manager.apply_upgrade(choices[index], player_stats)
+    difficulty_manager.elapsed_time = 30.0 * upgrade_manager._hit_count
     upgrade_manager.clear_pending()
     player_invulnerable_timer = player_invulnerable_duration
     state_manager.pop_state()  # Naase mängu olekusse - Return to PLAYING state
